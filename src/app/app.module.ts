ab549dd692fe821325395867f740508eeebf6172
Fl4v1o <flavio.thebigboss@gmail.com>
2018-03-28 23:54:29 -0300

    Inserção HighCharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,14 @@ import { FormsModule } from '@angular/forms';
 import { PersonService } from './services/person.service';
 import { PeopleComponent } from './people/people.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ChartComponent } from './people/chart/chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
     PeopleComponent,
+    ChartComponent,
     PageNotFoundComponent
   ],
   imports: [
